3bf004c075dd0b7d1e3453fd336b7399762418ab
glisA99 <ognjen.simic99@gmail.com>
2022-07-12 22:35:54 +0200

    Refactored AuthorizedRouteProxy

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import { LoginForm } from './components/LoginForm';
 import { Route, Routes } from 'react-router-dom';
+import { AuthorizedRouteProxy } from './components/AuthorizedRouteProxy';
 
 function App() {
 
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<LoginForm />}/>
+        <Route path='/' element={
+          <AuthorizedRouteProxy path=''>
+            <>INDEX</>
+          </AuthorizedRouteProxy>
+        }/>
         <Route path="/login" element={<>INDEX</>}/>
       </Routes>
     </div>
--- src/components/AuthorizedRouteProxy.tsx
+++ src/components/AuthorizedRouteProxy.tsx
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { WithChildren } from '../types/utils';
 import { AppContext, AppContextDispatch } from './withAppContext';
 import { USER_DATA } from './LoginForm';
 import { UserData } from '../api/login';
+import { Backdrop, CircularProgress } from '@mui/material';
 
 // path: route where to navigate if this route is restricted
 type AuthorizedRouteProps = WithChildren<{
@@ -22,15 +23,24 @@ export const AuthorizedRouteProxy = ({ children, path }: AuthorizedRouteProps) =
         // if there is no user data, restict route
         if (!user_data) {
             navigate(path);   
-            return;
+            return null;
         }
         const data: UserData = JSON.parse(user_data);
         appDispatch.loginUser({ username: data.username });
-    }
+    } 
+
+    const backdrop = (
+        <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={true}
+        >
+            <CircularProgress color="inherit" />
+        </Backdrop>
+    )
 
     return (
         <React.Fragment>
-            {children}
+            {appContext.user == undefined ? <>{backdrop}</> : children}
         </React.Fragment>
     )
 }
