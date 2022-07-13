0145faa8ac3e825dd753958eb6473d15dc125248
glisA99 <ognjen.simic99@gmail.com>
2022-07-14 00:12:55 +0200

    Added dtos

--- src/types/model.ts
+++ src/types/model.ts
@@ -12,3 +12,25 @@ export type User = {
     username: string
 }
 
+export type JedinicaMere = {
+    jedinicaMereId: number,
+    nazivJediniceMere: string
+}
+
+export type TipProizvoda = {
+    tipProizvodaId: number,
+    nazivTipaProizvoda: string
+}
+
+export type Proizvod = {
+    proizvodId: number,
+    nazivProizvoda: string,
+    datumProizvodnje: string,
+    cena: number,
+    vrstaProizvoda: string,
+    trenutnoStanjeZaliha: number,
+    jedinicaMere: JedinicaMere,
+    tipProizvoda: TipProizvoda,
+    hemijskeOsobine: Array<any>,
+    fizickeOsobine: Array<any>
+}
